858213bdedc73df1fa207b00e104d8ce101c35be
Harsh Vasava <hardyvsv9@gmail.com>
2026-03-12 18:32:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def login():
     return render_template("login.html")
 
 @app.route("/register")
-def registeration():
+def register():
     return render_template("register.html")
 
 if __name__ == "__main__":
